e68c7dcacbe4bf0f99de64804f663815beb41311
ilycazes <89310738+ilycazes@users.noreply.github.com>
2022-05-27 22:10:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const navMenuButton = document.getElementById("navButton");
 const cardDiv = document.getElementById("card");
-
+const nameDiv =document.getElementById("name");
 
 navMenuButton.addEventListener("click", () => {
+    cardDiv.style.display = cardDiv.style.display == "block" ? "none" : "block";
+  });
+  nameDiv.addEventListener("click", () => {
     cardDiv.style.display = cardDiv.style.display == "block" ? "none" : "block";
   });
